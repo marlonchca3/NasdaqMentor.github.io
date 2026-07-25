9b2b1f6c40ff56412cb6f7892cdf59e73f48b39c
marlonchca3 <marlonchca3@gmail.com>
2026-07-25 09:52:49 -0500

    24

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/NasdaqMentor.github.io/',
+  base: '/',
   plugins: [vue()],
   build: {
     target: 'es2020',
